63861496bb3112914ba6b44bcade609ec7db9b75
ALbert <albert.perez.toro@est.fib.upc.edu>
2017-06-19 13:44:51 +0200

    inter

--- Interoperability/inter.cpp
+++ Interoperability/inter.cpp
@@ -1,11 +1,14 @@
+#define GLEW_STATIC
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #include <stdio.h>
 #include "CL/cl.h"
 #include "CL/cl_gl.h"
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <GL/glx.h>
 #include <glm/glm.hpp>
 #include <iostream>
+#include <stdio.h>
 #include <vector>
 
 cl_context context;
@@ -14,16 +17,49 @@ cl_uint num_of_platforms=0;
 cl_platform_id platform_id;
 cl_device_id device_id;
 cl_uint num_of_devices=0;
+cl_kernel kernel;
 cl_mem mem;	
 size_t global;
 GLuint texture;
 
 
-int bufferSize = 0; //Now it is 0, but this variable will be determined by openCL size data.
-int tex_height=0, tex_width=0;  //Now it is 0, but this variable will be determined by openCL size data.
-std::vector< glm::vec3 > data; //Simulation of the data
+int bufferSize = 10; //Now it is 10, but this variable will be determined by openCL size data.
+int tex_height=10, tex_width=10;  //Now it is 0, but this variable will be determined by openCL size data.
+
+
+#define DATA_SIZE 10
+float inputData[DATA_SIZE]={4, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //simulation of the data
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 
 int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+   
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Interoperability", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    glewExperimental = GL_TRUE; 
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    } 
 	
 	if (clGetPlatformIDs(1, &platform_id, &num_of_platforms)!= CL_SUCCESS)
 	{
@@ -44,23 +80,19 @@ int main(){
 	};  
 
 	// try to get a supported GPU device
-	if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &num_of_devices) != CL_SUCCESS)
+	if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_CPU, 1, &device_id, &num_of_devices) != CL_SUCCESS)
 	{
 		printf("Unable to get device_id\n");
 		return 1;
 	}
 
-
 	//Creating the context and the queue
 	context = clCreateContext(props,1,&device_id,0,0,NULL);
 	queue = clCreateCommandQueue(context,device_id,CL_QUEUE_PROFILING_ENABLE,NULL);
 
-
-// Create 2D texture (OpenGL) 
-
+// 1.Create 2D texture (OpenGL) 
 	//generate the texture ID 
 	glGenTextures(1, &texture); 
-
 	//bdinding the texture and setting the
 	glBindTexture(GL_TEXTURE_2D, texture);
 
@@ -76,34 +108,34 @@ int main(){
 	//specify texture dimensions, format etc  (tiene tex_width & tex_height, maybe they shouldnt)
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
 
-
-	//Create the OpenGL Pixel-Buffer
+//2.Create the OpenGL Pixel-Buffer
 	GLuint pbo; 
 	glGenBuffers(1, &pbo);
 	glBindBuffer(GL_ARRAY_BUFFER, pbo); 
 	//specifying the buffer size, this will have to be done when we know the size of the data!!
-	glBufferData(GL_ARRAY_BUFFER, bufferSize, &data[0], GL_STATIC_DRAW);
-
-//Create the OpenCL buffer corresponding to the Pixel-Buffer-Object
+	glBufferData(GL_ARRAY_BUFFER, bufferSize, &inputData[0], GL_STATIC_DRAW);
+	
+//3.Create the OpenCL buffer corresponding to the Pixel-Buffer-Object	
 	mem = clCreateFromGLBuffer(context, CL_MEM_WRITE_ONLY, pbo,  NULL); 
 
-
-// Acquire  the ownership via clEnqueueAcquireGLObjects/
+//4.Acquire  the ownership via clEnqueueAcquireGLObjects & execute kernels that update the buffer content
+//4.1 Acquire the ownership via clEnqueueAcquireGLObjects:	
 	glFinish();
 	clEnqueueAcquireGLObjects(queue, 1,  &mem, 0, 0, NULL); 
+//4.2 Execute the OpenCL kernel that alters the buffer
+	clSetKernelArg(kernel, 0, sizeof(mem), &mem); 
+	//global=DATA_SIZE; 
+	clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global, NULL, 0, NULL, NULL); 
 
-//Modification of the openCLbuffer ??
-
-
-//Releasing the ownership via clEnqueueReleaseGLObjects
+//4.3 Releasing the ownership via clEnqueueReleaseGLObjects
 	clFinish(queue);
-	clEnqueueReleaseGLObjects(queue, 1,  &mem, 0, 0, NULL);
+	clEnqueueReleaseGLObjects(queue, 10,  &mem, 0, 0, NULL);
 
-
-//Finally,stream data from the PBO to the texture is required
+//5.Finally,stream data from the PBO to the texture is required
 	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, tex_width, tex_height, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+	std::cout << "TEXTURA:" << texture << std::endl;
 
 }
 

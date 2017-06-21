7bbcca764fb04f10bf96406baac5e1281ba7530e
ALbert <albert.perez.toro@est.fib.upc.edu>
2017-06-21 12:00:53 +0200

    Inter+test

--- Interoperability/inter.cpp
+++ Interoperability/inter.cpp
@@ -3,13 +3,18 @@
 #include <stdio.h>
 #include "CL/cl.h"
 #include "CL/cl_gl.h"
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <GL/glx.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <GL/glx.h>
+
 #include <iostream>
 #include <stdio.h>
 #include <vector>
+#include "shader.h"
 
 cl_context context;
 cl_command_queue queue;
@@ -17,23 +22,45 @@ cl_uint num_of_platforms=0;
 cl_platform_id platform_id;
 cl_device_id device_id;
 cl_uint num_of_devices=0;
-cl_kernel kernel;
+cl_kernel kernel_image;
+cl_program program;
 cl_mem mem;	
+cl_int err;
 size_t global;
 GLuint texture;
 
 
-int bufferSize = 10; //Now it is 10, but this variable will be determined by openCL size data.
-int tex_height=10, tex_width=10;  //Now it is 0, but this variable will be determined by openCL size data.
+const char *ProgramSource = 
+"__kernel void copy(__global float *mem)\n"\
+"{\n"\
+"  size_t id = get_global_id(0);\n"\
+"  mem[id] = mem[id];\n"\
+"}\n";
 
 
-#define DATA_SIZE 10
-float inputData[DATA_SIZE]={4, 2, 3, 4, 5, 6, 7, 8, 9, 10}; //simulation of the data
+int tex_height=2, tex_width=2;  //Now it is 0, but this variable will be determined by openCL size data.
+
+
+#define DATA_SIZE 4
+float inputData[DATA_SIZE]={65535.0f, 65535.0f, 65535.0f, 65535.0f}; //simulation of the data
+float results[DATA_SIZE]={0};
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+float texCoords[] = {
+    0.0f, 0.0f,  // lower-left corner  
+    0.0f, 1.0f,  // top-left corner
+    1.0f, 0.0f,   // lower-right corner
+    1.1f, 1.1f   // tope-right corner
+};
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+//void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
 int main(){
     glfwInit();
@@ -52,7 +79,14 @@ int main(){
         return -1;
     }
     glfwMakeContextCurrent(window);
-    
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+   // glfwSetCursorPosCallback(window, mouse_callback);
+   // glfwSetScrollCallback(window, scroll_callback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     glewExperimental = GL_TRUE; 
     // Initialize GLEW to setup the OpenGL Function pointers
     if (glewInit() != GLEW_OK)
@@ -93,7 +127,7 @@ int main(){
 // 1.Create 2D texture (OpenGL) 
 	//generate the texture ID 
 	glGenTextures(1, &texture); 
-	//bdinding the texture and setting the
+	//binding the texture and setting the
 	glBindTexture(GL_TEXTURE_2D, texture);
 
 	//regular sampler params
@@ -108,37 +142,141 @@ int main(){
 	//specify texture dimensions, format etc  (tiene tex_width & tex_height, maybe they shouldnt)
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex_width, tex_height, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
 
-//2.Create the OpenGL Pixel-Buffer
-	GLuint pbo; 
-	glGenBuffers(1, &pbo);
-	glBindBuffer(GL_ARRAY_BUFFER, pbo); 
-	//specifying the buffer size, this will have to be done when we know the size of the data!!
-	glBufferData(GL_ARRAY_BUFFER, bufferSize, &inputData[0], GL_STATIC_DRAW);
-	
-//3.Create the OpenCL buffer corresponding to the Pixel-Buffer-Object	
-	mem = clCreateFromGLBuffer(context, CL_MEM_WRITE_ONLY, pbo,  NULL); 
-
-//4.Acquire  the ownership via clEnqueueAcquireGLObjects & execute kernels that update the buffer content
-//4.1 Acquire the ownership via clEnqueueAcquireGLObjects:	
+//2.Create the OpenCL image corresponding to the texture
+	//mem = clCreateImage(context, CL_MEM_READ_WRITE, ,,CL_MEM_OBJECT_IMAGE2D,&	);
+	mem = clCreateFromGLTexture(context, CL_MEM_READ_WRITE, GL_TEXTURE_2D, 0,texture,&err);
+//3.Acquire  the ownership via clEnqueueAcquireGLObjects
 	glFinish();
 	clEnqueueAcquireGLObjects(queue, 1,  &mem, 0, 0, NULL); 
-//4.2 Execute the OpenCL kernel that alters the buffer
-	clSetKernelArg(kernel, 0, sizeof(mem), &mem); 
-	//global=DATA_SIZE; 
-	clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global, NULL, 0, NULL, NULL); 
+//4 Execute the OpenCL kernel that alters the image
+	//load data into the input buffer
+	size_t size3DOrig[3] = {0, 0, 0};
+	size_t size3D[3] = {DATA_SIZE, DATA_SIZE, 1};
+	clEnqueueWriteImage(queue, mem, CL_TRUE, size3DOrig, size3D,0 , 0, inputData, 0, NULL, NULL);
+	
+	kernel_image = clCreateKernel(program, "copy", &err);
+	clSetKernelArg(kernel_image, 1, sizeof(cl_mem), &mem); 
+	global=DATA_SIZE; 
+	clEnqueueNDRangeKernel(queue, kernel_image, 1, NULL, &global, NULL, 0, NULL, NULL); 
 
-//4.3 Releasing the ownership via clEnqueueReleaseGLObjects
+	
+//5. Releasing the ownership via clEnqueueReleaseGLObjects
 	clFinish(queue);
-	clEnqueueReleaseGLObjects(queue, 10,  &mem, 0, 0, NULL);
+	clEnqueueReleaseGLObjects(queue, 1,  &mem, 0, 0, NULL); 
+	
+	clEnqueueReadImage(queue, mem, CL_TRUE, size3DOrig, size3D,0 , 0, results, 0, NULL, NULL);
 
-//5.Finally,stream data from the PBO to the texture is required
-	glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, tex_width, tex_height, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-	std::cout << "TEXTURA:" << texture << std::endl;
+	// print the results
+	std::cout << "mem (values): ";
+	for(int i=0;i<DATA_SIZE; i++)
+	{
+		std::cout << results[i] << " ";
+	}	
+	std::cout << std::endl;
+	clReleaseMemObject(mem);
+	clReleaseProgram(program);
+	clReleaseKernel(kernel_image);
+	clReleaseCommandQueue(queue);
+	clReleaseContext(context);
+	
+	
+     // build and compile our shader zprogram
+    // ------------------------------------
+    Shader ourShader("inter.vs", "inter.fs");
+
+   // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+    // -------------------------------------------------------------------------------------------
+    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+    // either set it manually like so:
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+     // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // render container
+        ourShader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
 
 }
 
 
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
 
  

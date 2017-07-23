e32645ccb6867534bd531ab67f6a4ee2add4bcf8
ALbert <albert.perez.toro@est.fib.upc.edu>
2017-07-23 19:51:58 +0200

    Poniendo en loop

--- Interoperability/inter.cpp
+++ Interoperability/inter.cpp
@@ -37,6 +37,9 @@ std::string inputInitializeDepthBuffer = "initializeDepthBuffer.ocl";
 
 const char* inputDataFile = "escalaCol.obj";
 
+std::vector<float> v_vertexs;
+std::vector<float> speed;
+
 #define DATA_SIZE 1000000
 
 // settings
@@ -121,8 +124,7 @@ int main(int argc, char* argv[]){
 	readCompileKernelProgram(inputFragMerging, fragMerging);
 
 //Create a Vertex Buffer & Reading inputData
-	std::vector<float> v_vertexs;
-	std::vector<float> speed;
+
 	if(!obtainVertexs(inputDataFile,v_vertexs,speed))
 	{ 
 		std::cout<<"Error reading the input data file"<<std::endl;
@@ -165,6 +167,65 @@ int main(int argc, char* argv[]){
 	glFinish();
 	err = clEnqueueAcquireGLObjects(queue, 1,  &vertex_image, 0, 0, NULL); 
 	std::cout<<"ACQUIRE OWNERSHIP FROM GL: "<<  getErrorString(err)<< std::endl;
+	
+
+	// build and compile our shader zprogram
+	// ------------------------------------
+	Shader ourShader("inter.vs", "inter.fs");
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+	   // positions          // colors           // texture coords
+	    1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, // top right
+	    1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, // bottom right
+	   -1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, // bottom left
+	   -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f  // top left 
+	};
+
+
+	unsigned int indices[] = {
+	   0, 1, 3, // first triangle
+	   1, 2, 3  // second triangle
+	};
+	//  unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+	// -------------------------------------------------------------------------------------------
+	ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+	// either set it manually like so:
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+
+
+     // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+	    
+		// per-frame time logic
+		// --------------------
 	//4 Execute the OpenCL kernel that alters the image
 
 	//4.1 Calculating viewMatrix 
@@ -255,66 +316,7 @@ int main(int argc, char* argv[]){
 	//5. Releasing the ownership via clEnqueueReleaseGLObjects
 	clFinish(queue);
 	err = clEnqueueReleaseGLObjects(queue, 1,  &vertex_image, 0, 0, NULL); 
-	std::cout<<"RELEASING ENQUEUE OBJECTS: "<<  getErrorString(err)<< std::endl;	
-
-	// build and compile our shader zprogram
-	// ------------------------------------
-	Shader ourShader("inter.vs", "inter.fs");
-
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	// ------------------------------------------------------------------
-	float vertices[] = {
-	   // positions          // colors           // texture coords
-	    1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, // top right
-	    1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, // bottom right
-	   -1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, // bottom left
-	   -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f  // top left 
-	};
-
-
-	unsigned int indices[] = {
-	   0, 1, 3, // first triangle
-	   1, 2, 3  // second triangle
-	};
-	//  unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	// texture coord attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
-
-
-	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-	// -------------------------------------------------------------------------------------------
-	ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
-	// either set it manually like so:
-	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
-
-
-     // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-	    
-		// per-frame time logic
-		// --------------------
-
+	std::cout<<"RELEASING ENQUEUE OBJECTS: "<<  getErrorString(err)<< std::endl;
 		// input
 		// -----
 		processInput(window);
